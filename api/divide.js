43a7fd92f58d88bc8c0f2552eb62ca75e3f1d1bd
anamota13 <anajuliaalvesmota@gmail.com>
2025-05-21 17:42:08 -0300

    Atualizando

--- api/divide.js
+++ api/divide.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const { a, b } = req.query;
   if (!a || !b) return res.status(400).json({ error: 'Parâmetros obrigatórios' });
   if (parseFloat(b) === 0) return res.status(400).json({ error: 'Divisão por zero' });
